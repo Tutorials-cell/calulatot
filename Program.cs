1476ac3b4b7b5e07b2de5ecf516e444ba3c87a98
Bahulya <sarvalabahulya456@gmail.com>
2021-10-29 19:18:44 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            calculator objcalculator = new calculator();
+            Console.WriteLine("total values="+ objcalculator.issum(30,20));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo
+{
+   public class calculator
+    {
+        int totalValue;
+
+        public int issum(int num1, int num2)
+        {
+            return totalValue = num1 + num2;
+        }
+    }
+}
